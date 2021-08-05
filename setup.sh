88abfa67b5ab21641b093292e705b51ee18c5998
mangi07 <mangi07@users.noreply.github.com>
2021-08-05 17:03:40 +1000

    setting up dev intellisense

--- setup.sh
+++ setup.sh
@@ -1,3 +1,46 @@
+# If root on docker container:
+# Create user:
+#   adduser ben
+#   sudo usermod -aG sudo ben
+# cp this folder to user's home folder and ...
+# chown of this folder to ben user:
+#   sudo chown ben ./setup/
+
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install git
+sudo apt install curl
+sudo apt install tree
 sudo apt install htop
 sudo apt install tmux
+
 cp tmux/.tmux.conf.orig ~/.tmux.conf
+
+# python stuff
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9
+sudo apt install python3-pip
+pip3 install --upgrade pynvim
+
+
+# node stuff
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+# yarn package manager
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+     echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+     sudo apt-get update && sudo apt-get install yarn
+
+# nvim stuff
+sudo add-apt-repository ppa:neovim-ppa/unstable # needed to get neovim 0.5
+sudo apt-get update
+sudo apt-get install neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sudo chown -R $(whoami) /usr/lib/node_modules/
+npm install -g neovim
+npm install -g pyright
+
